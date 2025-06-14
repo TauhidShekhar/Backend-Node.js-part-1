5f29c1e2aae5b2f2a8b1650fd7fa1b3286918cc8
Tauhid Shekhar <161128044+TauhidShekhar@users.noreply.github.com>
2025-06-14 17:13:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// let n=5;
+// for(let i=1;i<=n; i++){
+//     console.log("Hello,",i);
+// }
+
+// console.log(process.argv);
+
+// let args=process.argv;
+// for(let i=2;i<args.length;i++){
+//     console.log("Hello to ",args[i]);
+// }
+
+// export in files
+// const math=require("./math.js");
+// console.log(math);
+// console.log(math.div(3,4));
+
+
+// export in directeries
+const fruitInfo=require("./fruits");
+console.log(fruitInfo)
---
+++ math.js
@@ -0,0 +1,27 @@
+const sum=(a,b)=> a+b;
+const sub=(a,b)=> a-b;
+const mul=(a,b)=> a*b;
+const div=(a,b)=> a/b;
+
+const pi=3.14;
+const count=88;
+
+//First trika to difin
+// let obj={
+//     sum:sum,
+//     sub:sub,
+//     div:div,
+//     mul:mul,
+//     pi:pi,
+//     count:count
+// }
+// module.exports=obj;
+
+module.exports={
+    sum,
+    sub,
+    mul,
+    div,
+    pi,
+    count
+};
